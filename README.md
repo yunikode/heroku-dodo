8cad78793decb75898517cec02699e703cac0fdd
zee-german <franz.lutzenberger@gmail.com>
2016-12-06 11:01:37 +0100

    added static todos and start routing

---
+++ README.md
@@ -0,0 +1,13 @@
+# Heroku Todo API
+
+First flight of the Dodo
+
+```
+yarn install
+```
+
+Server Up!
+
+```
+node server.js
+```
--- server.js
+++ server.js
@@ -3,8 +3,32 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+let todos = [{
+  id: 1,
+  description: 'Do the laundry',
+  completed: false
+},
+{
+  id: 2,
+  description: 'Go to market',
+  completed: false
+},
+{
+  id: 3,
+  description: 'Read the manual',
+  completed: true
+}]
+
 app.get('/', (req, res) => {
   res.send('Todo API Root')
+})
+
+app.get('/todos', (req, res) => {
+  res.json(todos)
+})
+
+app.get('/todos/:id', (req, res) => {
+
 })
 
 app.listen(PORT, () => console.log('Express listening on port ' + PORT) )
