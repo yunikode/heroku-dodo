c7e3d35cf318b992656bbf9e10b56062fc76412b
zee-german <franz.lutzenberger@gmail.com>
2016-12-12 15:01:56 +0100

    added postgres for production

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(undefined, undefined, undefined, {
-  'dialect': 'sqlite',
-  'storage': __dirname + '/data/dev-todo-api.sqlite'
-})
+const env = process.env.NODE_ENV || 'development'
+
+const sequelize
+
+if (env === 'production') {
+  sequelize = new Sequelize(process.env.DATABSE_URL, {
+    dialect: 'postgres'
+  })
+} else {
+  sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/data/dev-todo-api.sqlite'
+  }
+}
 
 const db = {}
 
